ae9677c58f9d2bcc5d2be189aebaaa834acf2224
1-gagandeep <112457883+1-gagandeep@users.noreply.github.com>
2023-08-02 13:14:23 +0530

    Updated

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import StackNavigator from './StackNavigator';
+import { FitnessContext } from './Context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <FitnessContext>
+
+        <StackNavigator/>
+      </FitnessContext>
+ 
   );
 }
 
@@ -14,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
---
+++ screens/HomeScreen.js
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from 'react-native'
+import React, { useContext } from 'react'
+import FitnessCards from '../components/FitnessCards'
+import { FitnessItems } from '../Context';
+
+const HomeScreen = () => {
+    const {completed,  minutes,  calories,  workout, } = useContext(FitnessItems);
+    return (
+        <ScrollView style={{marginTop:30}}>
+            
+                
+                <View style={{ backgroundColor: "#cd853f", padding: 10,  height:200}} >
+                    
+                
+                    <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>HOME WORKOUT</Text>
+
+                    <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", marginTop: 20 }} >
+                        <View>
+                            <Text style={{ textAlign: "center", fontWeight: "bold", color: "white", fontSize: 18 }} >{workout}</Text>
+                            <Text style={{ color: "#d0d0d0", fontSize: 17, marginTop: 6 }} >WORKOUTS</Text>
+                        </View>
+
+                        <View>
+                            <Text style={{ textAlign: "center", fontWeight: "bold", color: "white", fontSize: 18 }}>{calories}</Text>
+                            <Text style={{ color: "#d0d0d0", fontSize: 17, marginTop: 6 }}>KCAL</Text>
+                        </View>
+
+                        <View>
+                            <Text style={{ textAlign: "center", fontWeight: "bold", color: "white", fontSize: 18 }}>{minutes}</Text>
+                            <Text style={{ color: "#d0d0d0", fontSize: 17, marginTop: 6 }}>MINS</Text>
+                        </View>
+                    </View>
+                    <View style={{ justifyContent: "center", alignItems: "center" }} >
+                        <Image style={{ width: 330, height: 120, margin: 20 , borderRadius: 7, }} source={{ uri: "https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_842,ar_1.2,q_auto:eco,dpr_2,f_auto,fl_progressive/image/test/sku-card-widget/gold2.png" }} />
+                    </View>
+
+                    
+
+                </View>
+                <View style={{marginTop:50}} >
+                    <FitnessCards />
+
+                </View>
+          
+        </ScrollView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
+
